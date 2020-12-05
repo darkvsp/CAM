e1d524434aa567abc0b795b9e33d79fc9b0c4842
darkvsp <64093118+darkvsp@users.noreply.github.com>
2020-12-05 20:18:48 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,9 +5,11 @@ echo 'setting up....'
 printf '\e[0m'
 pkg install figlet termux-api -y
 printf '\e[92m'
-echo 'setup completed!'                                                       echo 'runnig installation....'
+echo 'setup completed!'                                                     
+echo 'runnig installation....'
 chmod +x cam.sh
 mv cam.sh /data/data/com.termux/files/usr/bin/
 clear
 figlet "" CAM
+echo "Use 'cam.sh' command for run the package"
 printf '\e[0m'
